6173b39e6805bc612baa04e0bc59d6fe9d7b4bfa
Aygün Bayır <turkoaygun@gmail.com>
2023-03-01 17:51:01 +0300

    added invoices page

--- src/scenes/invoices/index.tsx
+++ src/scenes/invoices/index.tsx
@@ -1,5 +1,45 @@
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
+
 const Invoices = () => {
-  return <div>Invoices</div>;
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    { field: "phone", headerName: "Phone Number", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color="secondary.main">${params.row.cost}</Typography>
+      ),
+    },
+    { field: "date", headerName: "Date", flex: 1 },
+  ];
+  return (
+    <Box m="20px">
+      <Header title="INVOICES" subtitle="List of Invoice Balances" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiCheckbox-root": {
+            color: "secondary.light !important",
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Invoices;
